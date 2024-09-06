72db7a636a4d482fb84d30065f01e8db158fbab3
MasoodahGeorge <oodasbusiness@gmail.com>
2024-09-06 08:38:50 +0200

    added

--- model/index.js
+++ model/index.js
@@ -1,10 +1,13 @@
 import { Users } from "./users.js";
 import { Products } from "./products.js";
+import { Cart } from "./cart.js";
 
 const usersFunc = new Users()
 const productsFunc = new Products()
+const cartFunc = new Products()
 
 export {
   usersFunc,
-  productsFunc
+  productsFunc,
+  cartFunc
 };
