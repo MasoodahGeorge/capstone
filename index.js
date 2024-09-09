c09c4fdb7491ee0e6e21512c8807a41208ef8e02
MasoodahGeorge <oodasbusiness@gmail.com>
2024-09-09 15:31:16 +0200

    added backend

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 import express from 'express';
+import path from 'path';
 import cartRoutes from './routes/cartRoute.js';
 import productRoutes from './routes/productsRoute.js';
 import userRoutes from './routes/usersRoute.js';
 import { errorHandling } from './middleware/errorHandling.js';
 
 const app = express();
+const port = +process.env.PORT || 8080;
 
-app.use(express.json());
+// Middleware for serving static files, JSON parsing, URL encoding
+app.use(
+    express.static(path.resolve(process.cwd(), 'static')),
+    express.json(),
+    express.urlencoded({
+        extended: true
+    })
+);
 
-// Routes
+// Define routes
 app.use('/cart', cartRoutes);
 app.use('/products', productRoutes);
 app.use('/users', userRoutes);
 
+// Serve the index.html file from the /static/html directory
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(process.cwd(), 'static', 'html', 'index.html'), (err) => {
+        if (err) {
+            res.status(500).send('Error loading index.html');
+        }
+    });
+});
+
+// Handle 404 errors for all other routes
+app.get('*', (req, res) => {
+    res.status(404).json({
+        status: 404,
+        msg: 'Resource not found'
+    });
+});
+
 // Error handling middleware (always at the end)
 app.use(errorHandling);
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
