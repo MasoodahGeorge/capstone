04c069288d0a26abce456516e98440a1e226227a
MasoodahGeorge <oodasbusiness@gmail.com>
2024-09-11 20:36:13 -0800

    msg

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.HOSTDB,
+    user: process.env.USERDB,
+    password: process.env.PASSWORDDB,
+    database: process.env.DATABASE_NAME
 });
 
 // Test the connection
